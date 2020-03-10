1a6407eab368072aa3baf74fff36200fc6b4bda7
weilh <13811469827@139.com>
2020-03-11 01:11:15 +0800

    提交冲突

--- Library/Core/InjectEntryPoint.cs
+++ Library/Core/InjectEntryPoint.cs
@@ -11,7 +11,7 @@ namespace RoeHack.Library.Core
     {
         private readonly ServerInterface server;
         private readonly IpcConnectLogger logger;
-
+        IDirectXHooker hooker;
         public InjectEntryPoint(RemoteHooking.IContext context, Parameter parameter)
         {
             server = RemoteHooking.IpcConnectClient<ServerInterface>(parameter.ChannelName);
