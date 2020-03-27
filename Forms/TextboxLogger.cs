dbc23cf8ad58bd0730dac041459295ef4635a6a7
七月 <mlacker@163.com>
2020-03-27 20:22:06 +0800

    feat(logger): add textbox logger for form

---
+++ Forms/TextboxLogger.cs
@@ -0,0 +1,42 @@
+﻿using RoeHack.Library.Core.Logging;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RoeHack.Forms
+{
+    public class TextboxLogger<T> : AbstractLogger
+    {
+        private readonly TextBoxBase textbox;
+        private readonly int maxLength;
+
+        public TextboxLogger(TextBoxBase textbox, LogLevel logLevel)
+            : base(typeof(T).Name, logLevel)
+        {
+            this.textbox = textbox;
+            this.maxLength = textbox.MaxLength;
+        }
+
+        public override void LogLevel(LogLevel level, string message, Exception exception = null)
+        {
+            var text = textbox.Text;
+            var stringBuilder = new StringBuilder(
+                text,
+                Math.Max(text.Length - maxLength, 0), 
+                Math.Min(text.Length, maxLength),
+                maxLength);
+
+            stringBuilder.AppendLine();
+            stringBuilder.Append($"[{level.ToString().ToUpper()}]".PadRight(8));
+            stringBuilder.Append(message);
+
+            if (exception != null)
+            {
+                stringBuilder.Append(Environment.NewLine)
+                    .Append(exception);
+            }
+
+            textbox.Text = stringBuilder.ToString();
+        }
+    }
+}
